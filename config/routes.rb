1deb9bd7b35310cb084c987e3dbb1c422c43f7f6
Van Nguyen <soutennizavan@gmail.com>
2013-06-04 15:44:05 -0400

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Sample::Application.routes.draw do
  
-  resources :users 
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end 
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+  
   root to: 'static_page#home'
  
   match '/signup', to: 'users#new'
